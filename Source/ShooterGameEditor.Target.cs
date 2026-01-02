dca38a834f4209bce932660c08fdeb99d026a1b9
mjchung <t7214948ttt@gmail.com>
2026-01-03 04:27:04 +0800

    upgrade to 5.7

--- Source/ShooterGameEditor.Target.cs
+++ Source/ShooterGameEditor.Target.cs
@@ -8,8 +8,8 @@ public class ShooterGameEditorTarget : TargetRules
 	public ShooterGameEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("ShooterGame");
 	}
 }
